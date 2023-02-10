9b417e02e889665ed7f6cc4a54b2d7e261fa5884
sharjeel <sharjeelahmadstanadar@gmail.com>
2023-02-11 02:05:25 +0500

    added card and data from firebase

--- lib/screens/veiw_all_record_student.dart
+++ lib/screens/veiw_all_record_student.dart
@@ -3,6 +3,7 @@ import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 
 import '../widgets/all_button_student.dart';
+
 class StudenRecord extends StatefulWidget {
   const StudenRecord({Key? key}) : super(key: key);
 
@@ -11,30 +12,34 @@ class StudenRecord extends StatefulWidget {
 }
 
 class _StudenRecordState extends State<StudenRecord> {
-  DatabaseReference? RecordRef ;
+  DatabaseReference? RecordRef;
 
   void initState() {
     user = FirebaseAuth.instance.currentUser;
     if (user != null) {
-      RecordRef =
-                  databaseReference.child("attendance").child(user!.uid);
+      RecordRef = databaseReference.child("attendance").child(user!.uid);
     }
+    chekc();
     super.initState();
   }
-  Future<void> chekc() async {
+
+  Future<List> chekc() async {
     List attandance = [];
-    DatabaseEvent event =
-    await FirebaseDatabase.instance.reference().child("attendance").child(user!.uid).once();
+    DatabaseEvent event = await FirebaseDatabase.instance
+        .reference()
+        .child("attendance")
+        .child(user!.uid)
+        .once();
     Object? value = event.snapshot.value;
+    print(value);
     Map<dynamic, dynamic> map = value as Map;
     map.forEach((key, value) {
-
       Map valueMap = value;
       for (var key in valueMap.keys) {
-
-        attandance.add(key);
+        attandance.add(valueMap[key]);
       }
     });
+    return attandance;
   }
 
   @override
@@ -44,8 +49,48 @@ class _StudenRecordState extends State<StudenRecord> {
         title: const Text('view Record'),
       ),
       body: Column(
-        children:  [
-          Text('no'),
+        children: [
+          Container(
+            width: double.infinity,
+            height: 500,
+            child: FutureBuilder(
+                future: FirebaseDatabase.instance
+                    .reference()
+                    .child("attendance")
+                    .child(user!.uid)
+                    .once(),
+                builder: (context, event) {
+                  if (event.hasData) {
+                    List attendanceValues = [];
+                    List attendanceKeys = [];
+                    List days = [];
+                    Object? value = event.data?.snapshot.value;
+                    print(value);
+                    Map<dynamic, dynamic> map = value as Map;
+                    map.forEach((key, value) {
+                      Map valueMap = value;
+                      days.add(key);
+                      for (var key in valueMap.keys) {
+                        attendanceValues.add(valueMap[key]);
+                        attendanceKeys.add(key);
+                      }
+                    });
+                    return ListView.builder(
+                        itemCount: attendanceValues.length,
+                        itemBuilder: (context, index) {
+                          return Card(
+                            child: ListTile(
+                              title: Text(attendanceValues[index]),
+                              subtitle: Text(attendanceKeys[index]),
+                              trailing: Text(days[index]),
+                            ),
+                          );
+                        });
+                  } else {
+                    return const Center(child: CircularProgressIndicator());
+                  }
+                }),
+          ),
         ],
       ),
     );
